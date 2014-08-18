112ebcb10ab81017cbc97808ba1ba578bbd0bb0a
sam <sam@sphericalcommunications.com>
2014-08-18 18:00:18 -0400

    GA

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -116,20 +116,6 @@ var google_remarketing_only = true;
 </div>
 </noscript>
 
-<script type="text/javascript">
-
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-37161096-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	</script>
-
 
 <script type="text/javascript">
 
