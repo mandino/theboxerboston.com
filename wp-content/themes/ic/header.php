e97910e5170ea32f969c71bea0fcf3949be19a5f
Mandino <dino@misfit-inc.com>
2015-03-12 19:19:35 +0800

    add new tracking

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -91,22 +91,6 @@ var google_remarketing_only = true;
 </div>
 </noscript>
 
-<script>
-
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-37161096-1', 'auto', {
-		'allowLinker': true
-	});
-	ga('send', 'pageview');
-	ga('require', 'linker');
-	ga('linker:autoLink', ['reztrip.com'], false, true);
-
-</script>
-
 <!-- zdirect script -->
 
 <script type="text/javascript" src="https://www.zdirect.com/scripts/newApp.js"></script>
@@ -143,6 +127,22 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3035';(document.getElementsByTagName
 })();
 </script>
 <!-- End Sojern -->
+
+<script>
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-37161096-1', 'auto', {
+		'allowLinker': true
+	});
+	ga('send', 'pageview');
+	ga('require', 'linker');
+	ga('linker:autoLink', ['reztrip.com'], false, true);
+
+</script>
 
 </head> 
 	
