1159758c9751e0955c4d8e9718f237300c97c9cc
Mandino <dino@misfit-inc.com>
2016-03-18 11:37:19 +0800

    add facebook pixel

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -98,16 +98,16 @@
 		wp_head();
 	?>
 	
-<!-- Sojern Head -->
-<script>
-(function () {
-var pl = document.createElement('script');
-pl.type = 'text/javascript';
-pl.async = true;
-pl.src = 'https://beacon.sojern.com/pixel/p/3035';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
-})();
-</script>
-<!-- End Sojern -->
+	<!-- Sojern Head -->
+	<script>
+	(function () {
+	var pl = document.createElement('script');
+	pl.type = 'text/javascript';
+	pl.async = true;
+	pl.src = 'https://beacon.sojern.com/pixel/p/3035';(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(pl);
+	})();
+	</script>
+	<!-- End Sojern -->
 
 	<script type="application/ld+json">
 		{
@@ -123,7 +123,22 @@ pl.src = 'https://beacon.sojern.com/pixel/p/3035';(document.getElementsByTagName
 		"description": "A most wonderful article",
 		"articleBody": "The full body of the article"
 		}
-	</script> 
+	</script>
+
+	<!-- Facebook Pixel Code -->
+	<script>
+	!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+	document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+	fbq('init', '164851350567642');
+	fbq('track', "PageView");</script>
+	<noscript><img height="1" width="1" style="display:none"
+	src="https://www.facebook.com/tr?id=164851350567642&ev=PageView&noscript=1"
+	/></noscript>
+	<!-- End Facebook Pixel Code -->
 	
 
 </head> 
