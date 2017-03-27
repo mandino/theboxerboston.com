da1f69c262c576dbf36110e72cc788cfaa13ab49
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2017-03-27 11:04:34 +0800

    neighborhood edit

--- wp-content/themes/ic/page_guide.php
+++ wp-content/themes/ic/page_guide.php
@@ -315,7 +315,7 @@
 						<div class="container">
 							
 								
-							<?php query_posts('post_type=page&p=178'); if(have_posts()) : while(have_posts()) : the_post(); ?>
+							<?php query_posts('post_type=page&p=72'); if(have_posts()) : while(have_posts()) : the_post(); ?>
 							
 							<?php the_content(); ?>
 							
@@ -330,7 +330,7 @@
 								
 								<div class="townbox">
 								
-									<?php query_posts('post_type=page&p=78'); if(have_posts()) : while(have_posts()) : the_post(); ?>
+									<?php query_posts('post_type=page&p=72'); if(have_posts()) : while(have_posts()) : the_post(); ?>
 									
 									
 									<ul>
