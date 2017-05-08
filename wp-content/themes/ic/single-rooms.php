1283b44fc56dbe8c5dfa3475218ff868e0e41c2c
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2017-05-08 19:29:35 +0800

    single room edit 2

--- wp-content/themes/ic/single-rooms.php
+++ wp-content/themes/ic/single-rooms.php
@@ -40,7 +40,45 @@
 		
 		<div class="container">
 
+			<div class="section-header">
+					
+				<div class="fl">
+	
+					<?php if(get_option('cebo_shorttitle')) { ?>
+					
+					<div class="section-pre-title fl"><?php echo get_option('cebo_shorttitle'); ?></div>
+
+					<div class="section-header-divider fl"></div>
+					
+					<?php } ?>
+
+		
+					<h1 class="section-title fr"><?php the_title(); ?></h1>
+	
+				</div>
+	
+				<div class="fr">
+					
+					<ul class="social-buttons">
+					<?php if(get_option('cebo_facebook')) { ?>
+					
+						<li class="facebook"><a href="//facebook.com/<?php echo get_option('cebo_facebook'); ?>" target="_blank"><i class="fa fa-facebook fa-2x"></i><span>facebook</span></a></li>
+						
+					<?php } ?>
+					<?php if(get_option('cebo_twitter')) { ?>
+					
+						<li class="twitter"><a href="//twitter.com/<?php echo get_option('cebo_twitter'); ?>" target="_blank"><i class="fa fa-twitter fa-2x"></i><span>twitter</span></a></li>
+						
+					<?php } ?>
+					</ul>
+	
+				</div>
+	
+			</div>
+
 			<div class="wonderline"></div>
+
+			<div class="breadcrumbs"><a href="<?php bloginfo('url'); ?>/accommodations">All Rooms</a> > <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
 			
 			<div class="room-details-content">
 
