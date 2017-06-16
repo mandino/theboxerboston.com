b341f3a2e397ca925da93afa2f54b220faec30fa
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2017-06-16 14:51:09 +0800

    remove ga code

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -105,14 +105,6 @@
 	})(window,document,'script','dataLayer','GTM-5XBBBWD');</script>
 	<!-- End Google Tag Manager -->
 
-	<script> 
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new
-		Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-37161096-1', 'auto',{'allowLinker': true });
-		ga('require', 'linker'); ga('linker:autoLink', ['theboxerboston.reztrip.com','theboxerboston.reztripmobile.com']);
-		ga('send', 'pageview');
-	</script>
-
 	<!-- Sojern Head -->
 	<script>
 		(function () {
