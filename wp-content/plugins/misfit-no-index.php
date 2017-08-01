27c1e214b2dbcaf93eeba101f6814c6c5dd16289
jonathan-misfitcdo <jonathan.misfitcdo@gmail.com>
2017-08-01 20:42:44 +0800

    code update

--- wp-content/plugins/misfit-no-index.php
+++ wp-content/plugins/misfit-no-index.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: No Index on Events Calendar Past Events
-Plugin URI:  http://misfit-inc.com/
+Plugin URI:  http://misfit-inc.com
 Description: Automatically add a "noindex" meta tag to past events on event calendar' detail pages to prevent them from appearing in search engines.
 Version:     1.0
 Author:      Misfit Inc
-Author URI:  http://misfit-inc.com/
+Author URI:  http://misfit-inc.com
 Text Domain: misfit
 License:     GPL2
 */
@@ -15,42 +15,30 @@ if (!defined('ABSPATH')) { exit; }
 // added no index when Yoast SEO disable
 function misfit_noindex_past_events() {
 	global $post;
-    global $wp;
-  
     $event= tribe_get_events_link ();
 	if (!is_admin() && $post->post_type == 'tribe_events') {
-        remove_yoast_canonical();
-       echo "<link rel=\"canonical\" href=\"".$event."\" />";
 		if (!function_exists('tribe_get_end_date')) { return false; }          
                 if(tribe_is_day() === true) {
-                    echo "\n<!-- noindex Past Events -->\n<meta name=\"robots\" content=\"noindex, follow\" />\n\n";  
-                }
-            if(tribe_is_list_view() == false ){
-                     $linkn = tribe_get_next_event_link();
-                   if($linkn){
-                        preg_match_all('/<a[^>]+href=([\'"])(?<href>.+?)\1[^>]*>/i', $linkn, $resultn);
-                        $relnext = $resultn['href'][0];
-                       echo "\n<link rel=\"next\" href=\"".$relnext."\" />\n";
-                   }
-                    $linkp = tribe_get_prev_event_link();
-                   if($linkp){
-                        preg_match_all('/<a[^>]+href=([\'"])(?<href>.+?)\1[^>]*>/i', $linkp, $resultp);
-                        $relprev = $resultp['href'][0];
-                       echo "\n<link rel=\"prev\" href=\"".$relprev."\" />\n";
-                   }
+                    
+                    echo "\n<!-- noindex calendar Events -->\n<meta name=\"robots\" content=\"noindex, follow\" />\n\n";  
+                    
+                    $t_url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+                    $turl = explode('/',$t_url);
+                    $turl = array_filter($turl);                       
+                    $pdate = end($turl);
+    
+                    $prev_date = date('Y-m-d', strtotime($pdate.' -1 day'));
+                    $prev_link = $event.$prev_date.'/';
+                    $next_date = date('Y-m-d', strtotime($pdate.' +1 day'));
+                    $next_link = $event.$next_date.'/';
+                    echo "\n<link rel=\"next\" href=\"".$next_link."\" />\n";
+                    echo "\n<link rel=\"prev\" href=\"".$prev_link."\" />\n";
+
                   
             }
 	}
 }
 add_action('wp_head', 'misfit_noindex_past_events');
-
-
-//* remove yoast added canonical tag on tribe events
-function remove_yoast_canonical(){
-    return false;
-}
-
-add_filter( 'wpseo_canonical', 'remove_yoast_canonical' );
 
 // Add admin page
 function misfit_admin() {
