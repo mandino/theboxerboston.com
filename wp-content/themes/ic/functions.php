bc33c79692f9ff24f1c5a592ebf4d01ba039759b
arvinsumohid <arvinsumohid99@gmail.com>
2017-11-14 19:49:26 +0800

    add thumbnail uploader in events

--- wp-content/themes/ic/functions.php
+++ wp-content/themes/ic/functions.php
@@ -284,3 +284,73 @@ $class_even = $class_even ? '' : 'class="even"';
 
 endwhile; endif; wp_reset_postdata(); wp_die();
 }
+
+// Image with src, srcset and sizes $class => image class
+
+function get_image_with_detail( $photo_id, $size = 'Full' ) {
+	$imgsrc = wp_get_attachment_image_src( $photo_id, $size);
+	$srcset = esc_attr( wp_get_attachment_image_srcset( $photo_id, $size ) );
+	$imgsize = esc_attr( wp_get_attachment_image_sizes( $photo_id, $size ) );
+
+	return array( 'imgsrc' => $imgsrc, 'srcset' => $srcset, 'sizes' => $imgsize );
+}
+
+function custom_tribe_event_thumbnail_image( $post_id = null, $size = 'full', $link = true, $wrapper = true ) {
+	if ( is_null( $post_id ) ) {
+		$post_id = get_the_ID();
+	}
+
+	/**
+	 * Provides an opportunity to modify the featured image size.
+	 *
+	 * @param string $size
+	 * @param int    $post_id
+	 */
+	$size = apply_filters( 'tribe_event_featured_image_size', $size, $post_id );
+
+	$featured_image = $wrapper
+		? get_the_post_thumbnail( $post_id, $size )
+		: wp_get_attachment_image_src( get_post_thumbnail_id( $post_id ), $size, false );
+
+	if ( is_array( $featured_image ) ) {
+		$featured_image = $featured_image[ 0 ];
+	}
+
+	/**
+	 * Controls whether the featured image should be wrapped in a link
+	 * or not.
+	 *
+	 * @param bool $link
+	 */
+
+	if (get_field('thumbnail')) :
+		$image_detail = get_image_with_detail( get_field('thumbnail'), $size );
+		$banner_photo = $image_detail['imgsrc'];
+		$banner = $banner_photo;
+		$featured_image = '<img src="'.$banner[0].'" class="attachment-full size-full wp-post-image" srcset="'.$image_detail['srcset'].'" sizes="'.$image_detail['sizes'].'" alt="'.get_the_title().'">';
+
+	endif;
+
+	if ( ! empty( $featured_image ) && apply_filters( 'tribe_event_featured_image_link', $link ) ) {
+		$featured_image = '<a href="' . esc_url( tribe_get_event_link( $post_id ) ) . '">' . $featured_image . '</a>';
+	}
+
+	/**
+	 * Whether to wrap the featured image in our standard div (used to
+	 * assist in targeting featured images from stylesheets, etc).
+	 *
+	 * @param bool $wrapper
+	 */
+	if ( ! empty( $featured_image ) && apply_filters( 'tribe_events_featured_image_wrap', $wrapper ) ) {
+		$featured_image = '<div class="tribe-events-event-image">' . $featured_image . '</div>';
+	}
+
+	/**
+	 * Provides an opportunity to modify the featured image HTML.
+	 *
+	 * @param string $featured_image
+	 * @param int    $post_id
+	 * @param string $size
+	 */
+	return apply_filters( 'tribe_event_featured_image', $featured_image, $post_id, $size );
+}
