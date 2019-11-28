d9dcc2462a723463db72facf8904737c35b13d87
nicosabio2016 <nico@misfit-inc.com>
2019-11-28 18:57:23 +0800

    New SSO Feature
    Bug Fix

--- wp-content/themes/ic/header.php
+++ wp-content/themes/ic/header.php
@@ -270,15 +270,56 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					<li class="blue-btn"><a href="//theboxerboston.com/blue"><i class="fa fa-info-circle"></i><span class="blue-mobile">why blue?</span></a></li>
 				</ul>
 			</nav>
-			
-			
-			
-	
+		</div>
+
+		<div class="login-portal" aria-label="Membership Portal" aria-modal="true" tabindex="0">
+			<form id="login-portal">
+				<input type="text" name="fname" placeholder="First Name">
+				<input type="text" name="lname" placeholder="Last Name">
+				<input type="text" name="email" placeholder="Email">
+				<input type="text" name="zipcode" placeholder="Zip Code">
+				<span class="btn--circle">
+					<input type="submit" name="submit" value="Join Us">
+				</span>	
+			</form>
+			<div class="postal-greetings">
+				<span>Your Perks are Waiting</span>
+			</div>
+			<span class="postal-signin">Already a Member? <a href="https://independentcollectionloyaltyqa.cendyn.com/">Sign In</a></span>
 		</div>
 	
 		<div id="primary-nav" style="overflow:visible;">
-		
+			<div class="login-portal__trigger--container">
+				<button class="login-portal__trigger">
+					<span class="login-portal__text">Sign In / Sign Up</span>
+					<div class="login-portal__close-btn">
+						<span class="line-one"></span>
+						<span class="line-two"></span>
+					</div>
+					
+				</button>
+			</div>
             <a href="<?php bloginfo('url'); ?>" class="logo droplogo"><img src="<?php echo get_option('cebo_logo'); ?>" alt="<?php echo the_title(); ?>" /></a>
+
+            <div class="login-portal__trigger--container">
+				<button class="login-portal__trigger--secondary desktop">
+					<span class="login-portal__text">Sign In / Sign Up</span>
+					<div class="login-portal__close-btn">
+					<span class="line-one"></span>
+					<span class="line-two"></span>
+				</div>
+				</button>
+			</div>
+			
+			<div class="login-portal__trigger--container">
+				<button class="login-portal__trigger--secondary mobile">
+					<span class="login-portal__text">Sign In / Sign Up</span>
+					<div class="login-portal__close-btn">
+					<span class="line-one"></span>
+					<span class="line-two"></span>
+				</div>
+				</button>
+			</div>
             
 			<a href="<?php bloginfo('url'); ?>" class="logo mobile"><img src="<?php echo get_option('cebo_logo'); ?>" alt="<?php echo the_title().'-mobile'; ?>" /></a>
 			
--- wp-content/themes/ic/library/scripts.php
+++ wp-content/themes/ic/library/scripts.php
@@ -84,6 +84,60 @@
 		});
 	}
 	$(document).ready(function(){
+
+
+		$(document).on('keydown', function(e) {			
+			var input = $('#login-portal').find('input');
+			if ((e.keyCode || e.which) == 9) {
+	        	var focused = $(':focus');
+		        if ($(focused).hasClass('login-portal__trigger')) {  	
+		        	if ($('.login-portal').is(':visible')) {
+		        		
+						$('.login-portal').focus();
+					}
+		        }
+	    	}
+		    
+		});
+		
+		$('.login-portal__trigger--container, .click-nav ul li').on('click touchstart', function(e) {
+			
+			if (!$('body').hasClass('portal-active')) {
+				if (!$('.login-portal').is(':visible')) {
+					$('.login-portal').slideDown(1000);
+					$('#primary-nav').addClass('open-portal');
+					$('.login-portal__text').addClass('hidden');
+					$('.login-portal__close-btn').addClass('show');
+					$('body').addClass('portal-active');
+					if (e.keyCode === 13) {
+						if ($('.login-portal').is(':visible')) {
+							$('#login-portal').find('input[name="fname"]').addClass('first-item');
+							$('.login-portal').focus();
+						}
+					}
+
+					
+				}
+			} else {
+
+				if ($('.login-portal').is(':visible')) {
+					$('.login-portal').slideUp(1000);
+					$('#primary-nav').removeClass('open-portal');
+					$('.login-portal__text').removeClass('hidden');
+					$('.login-portal__close-btn').removeClass('show');
+					$('body').removeClass('portal-active');
+				}
+				if (e.keyCode === 13) {
+					if ($('.login-portal').is(':visible')) {
+						$('body').removeClass('portal-active');
+					}
+				}
+			}
+
+
+	
+		});
+
 		pressLogoDisplay()
 		$(".eat-tab-button").click(function() {
 			var id = "#"+$(this).data('class');
@@ -522,12 +576,28 @@
     
       if (verschil > 40) 
             
-           $('.droplogo').addClass('jumpshot');
+        $('.droplogo').addClass('jumpshot');
+    	if ($('.login-portal__trigger--secondary').hasClass('desktop')) {
+			$('.login-portal__trigger--secondary.desktop').addClass('active');
+        	$('.login-portal__trigger').addClass('inactive');
+		}
+
+		if ($('.login-portal__trigger--secondary').hasClass('mobile') && $('.login-portal__trigger--secondary').is(':visible')) {
+			$('.login-portal__trigger--secondary.mobile').addClass('active');
+        	$('.login-portal__trigger').addClass('inactive');
+		}
               
         
         else if (verschil < 40)
             
-           $('.droplogo').removeClass('jumpshot');
+           	$('.droplogo').removeClass('jumpshot');
+       		$('.login-portal__trigger--secondary.desktop').removeClass('active');
+	       	$('.login-portal__trigger').removeClass('inactive');
+
+	       	if ($('.login-portal__trigger--secondary').hasClass('mobile')) {
+    			$('.login-portal__trigger--secondary.mobile').removeClass('active');
+	        	$('.login-portal__trigger').removeClass('inactive');
+    		}
     });
     
 //    countdown picker
